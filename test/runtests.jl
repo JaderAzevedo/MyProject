e4c05e680bb9da1963418649736688f9e89217b2
JaderAzevedo <34697042+JaderAzevedo@users.noreply.github.com>
2024-01-13 16:57:57 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Test
 @testset "Testing MyProject.jl" begin
     # Write your tests here.
     # @test operateOnXY() == 2
-    @test MyProject.operateOnXY() == 2
+    @test operateOnXY() == 2
 
 end
